ecaf6e05868d8b6a8e59e1f14df5a125682483ff
anishbadri@gmail.com <anishbadri@gmail.com>
2019-04-04 18:58:55 +0530

    Day 7

--- track/routes.py
+++ track/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from track.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
+from track.forms import RegistrationForm, LoginForm, ProfileUpdateForm
 from track import app, bcrypt, db
 from track.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -49,14 +49,39 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            flash('Successfully logged in', 'Success')
-            return redirect(url_for('hello'))
+            next_page = request.args.get('next')
+            flash('Successfully logged in', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('hello'))
         else:
             flash('Something wrong with your password or email', 'danger')
     return render_template('login.html', title='Login Page', form = form)
 
 
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        return redirect(url_for('home'))
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('hello'))
+
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = ProfileUpdateForm()
+    # form.username.data = current_user.username
+    # form.email.data = current_user.email
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        # db.session.add(current_user)
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('profile.html', title='Profile Page', image_file= image_file, form=form)
+
